83767234360be6491ae800b086e6abe8f9d72919
Akhmadovich03 <akhmadovich03@gmail.com>
2024-07-28 22:06:24 +0500

    Final change before submitting

--- DrawingBoard/Data/BoardDbContext.cs
+++ DrawingBoard/Data/BoardDbContext.cs
@@ -5,11 +5,11 @@ namespace DrawingBoard.Data;
 
 public class BoardDbContext : DbContext
 {
+    public DbSet<Board> Boards { get; set; }
+    public DbSet<DrawingElement> DrawingElements { get; set; }
+
     public BoardDbContext(DbContextOptions<BoardDbContext> options) : base(options)
     {
         //Database.Migrate();
     }
-
-    public DbSet<Board> Boards { get; set; }
-    public DbSet<DrawingElement> DrawingElements { get; set; }
 }
